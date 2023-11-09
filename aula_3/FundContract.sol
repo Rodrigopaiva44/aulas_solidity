47f4eeb8dd6205655da9bbedf6c41536fde6180b
Rodrigo Matheus de A. Paiva <73554373+Rodrigopaiva44@users.noreply.github.com>
2023-11-09 07:20:56 -0300

    lesson 3 - 2/2

--- aula_3/FundContract.sol
+++ aula_3/FundContract.sol
@@ -1,30 +1,41 @@
 contract FundContract{
+    using PriceConverter for uint256;
 
     uint256 public minUsd = 5e18;
-    
-    function fund() public payable  {   
-        require(msg.value > minUsd, "Not enough money"); 
-    }   
+    address[] public funders;
+    mapping(address funder => uint256 amountFunded) public addressToAmountFunded;    // in solidity we can specify names to our mapping 
+ 
 
-    // An example 
-    function getVersion() public view returns(uint256){
-        return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
+    address public owner;
+    constructor() {
+        owner = msg.sender; // msg.sender is who deployed the contract
     }
 
-    function getPrice() public view returns(uint256){
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        (, int256 price,,,) = priceFeed.latestRoundData();
-        // price of ETH in terms of USD
-        // price have 10 decimals here and msg.value have 18 decimals so we need to fix that:
-        return uint256(price) * 1e10; // view returns(uint256);
-    } 
 
-    function getConvertionRate(uint256 ethAmount) public view returns(uint256) {
-        uint256 ethPrice = getPrice();
-        // multiplying 10^18 * 10^18 = 10^36 thats why we divide for 1e18
-        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;  (in solidity we awals want to multiple first)
-        return ethAmountInUsd;
-    } 
+    function fund() public payable {   
 
+        require(msg.value.getConvertionRate() >= minUsd, "Not enough money");
 
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
+    }   
+    function withdraw() public onlyOwner {
+        
+        for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++){
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+
+        funders = new address[](0);
+        // tansact
+        // send
+        // call 
+        (bool callSucess,) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSucess,"Call failed");
+    }
+    modifier onlyOwner(){
+        require(msg.sender == owner,"Just the owner can call this function");
+        _; 
+        // the underline means the rest of the function code, if we put it first, will run the code and require after.
+    }
 }
