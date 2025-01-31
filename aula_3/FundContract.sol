ecb2985f4b2140bacfe5a27198d53395f9f90bce
Rodrigo Matheus de A. Paiva <73554373+Rodrigopaiva44@users.noreply.github.com>
2025-01-31 00:59:26 -0300

    Update FundContract.sol

--- aula_3/FundContract.sol
+++ aula_3/FundContract.sol
@@ -1,45 +1,87 @@
-pragma solidity ^0.8.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
 
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 import {PriceConverter} from "./PriceConverter.sol";
- 
-contract FundContract{
+
+error NotOwner();
+
+contract FundMe {
     using PriceConverter for uint256;
 
-    uint256 public minUsd = 5e18;
+    mapping(address => uint256) public addressToAmountFunded;
     address[] public funders;
-    mapping(address funder => uint256 amountFunded) public addressToAmountFunded;    // in solidity we can specify names to our mapping 
- 
 
-    address public owner;
+
+    address public /* immutable */ i_owner;
+    uint256 public constant MINIMUM_USD = 5 * 10 ** 18;
+
     constructor() {
-        owner = msg.sender; // msg.sender is who deployed the contract
+        i_owner = msg.sender;
     }
 
-
-    function fund() public payable {   
-
-        require(msg.value.getConvertionRate() >= minUsd, "Not enough money");
-
+    function fund() public payable {
+        require(msg.value.getConversionRate() >= MINIMUM_USD, "Thats not enough");
+        // require(PriceConverter.getConversionRate(msg.value) >= MINIMUM_USD, "Thats not enough");
+        addressToAmountFunded[msg.sender] += msg.value;
         funders.push(msg.sender);
-        addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
-    }   
+    }
+
+    function getVersion() public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        return priceFeed.version();
+    }
+
+    modifier onlyOwner() {
+        // require(msg.sender == owner);
+        if (msg.sender != i_owner) revert NotOwner();
+        _;
+    }
+
     function withdraw() public onlyOwner {
-        
-        for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++){
+        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
             address funder = funders[funderIndex];
             addressToAmountFunded[funder] = 0;
         }
-
         funders = new address[](0);
-        // transact
-        // send
-        // call 
-        (bool callSucess,) = payable(msg.sender).call{value: address(this).balance}("");
-        require(callSucess,"Call failed");
+        // // transfer
+        // payable(msg.sender).transfer(address(this).balance);
+
+        // // send
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "Send failed");
+
+        // call
+        (bool callSuccess,) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
     }
-    modifier onlyOwner(){
-        require(msg.sender == owner,"Just the owner can call this function");
-        _; 
-        // the underline means the rest of the function code, if we put it first, will run the code and require after.
+
+    // Explainer from: https://solidity-by-example.org/fallback/
+    // Ether is sent to contract
+    //      is msg.data empty?
+    //          /   \
+    //         yes  no
+    //         /     \
+    //    receive()?  fallback()
+    //     /   \
+    //   yes   no
+    //  /        \
+    //receive()  fallback()
+
+    fallback() external payable {
+        fund();
+    }
+
+    receive() external payable {
+        fund();
     }
 }
+
+// Some important topics
+// 1. Enum
+// 2. Events
+// 3. Try / Catch
+// 4. Function Selector
+// 5. abi.encode / decode
+// 6. Hash with keccak256
+// 7. Yul / Assembly
